d5ce9a0a100ebb291f446551807cddecc908a96e
Tanmay <pateltanmay73@gmail.com>
2021-06-10 23:54:08 +0530

    Seventy Percent

--- chatgui.py
+++ chatgui.py
@@ -3,7 +3,6 @@ from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-
 from keras.models import load_model
 model = load_model('chatbot_model.h5')
 import json
@@ -18,29 +17,22 @@ def clean_up_sentence(sentence):
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
     bag = [0]*len(words)
     for s in sentence_words:
         for i,w in enumerate(words):
-            if w == s:
-                # assign 1 if current word is in the vocabulary position
+            if w == s:                
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
     return(np.array(bag))
 
-def predict_class(sentence, model):
-    # filter out predictions below a threshold
+def predict_class(sentence, model):    
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
@@ -61,11 +53,8 @@ def chatbot_response(msg):
     res = getResponse(ints, intents)
     return res
 
-
-#Creating GUI with tkinter
 import tkinter
 from tkinter import *
-
 
 def send():
     msg = EntryBox.get("1.0",'end-1c').strip()
@@ -88,26 +77,18 @@ base.title("Hello")
 base.geometry("400x500")
 base.resizable(width=FALSE, height=FALSE)
 
-#Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
-
+ChatLog = Text(base, bd=0, bg="black", height="8", width="50", font="Arial",)
 ChatLog.config(state=DISABLED)
 
-#Bind scrollbar to Chat window
 scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
 ChatLog['yscrollcommand'] = scrollbar.set
 
-#Create Button to send message
-SendButton = Button(base, font=("FiraCode",12,'bold'), text="Send", width="12", height=5,
+SendButton = Button(base, font=("JetBrains Mono",12,'bold'), text="Send", width="12", height=5,
                     bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
                     command= send )
-
-#Create the box to enter message
+                    
 EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
-#EntryBox.bind("<Return>", send)
 
-
-#Place all components on the screen
 scrollbar.place(x=376,y=6, height=386)
 ChatLog.place(x=6,y=6, height=386, width=370)
 EntryBox.place(x=128, y=401, height=90, width=265)
